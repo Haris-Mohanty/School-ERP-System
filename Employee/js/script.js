1f8f946bd0cc2eb0eb88d36b2974f79da4401334
Haris Mohanty <harismohanty8658@gmail.com>
2023-03-04 19:22:06 +0530

    EMPLOYE page:- Institue Update collapseble code fixed.

---
+++ Employee/js/script.js
@@ -0,0 +1,7 @@
+//INSTITUTE UPDATE COLLAPSEBLE CODE START
+$(document).ready(function () {
+  $(".institute-update-btn").click(function () {
+    $(".institute-menu").collapse("toggle");
+  });
+});
+//INSTITUTE UPDATE COLLAPSEBLE CODE END
