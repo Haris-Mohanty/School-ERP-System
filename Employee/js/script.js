07cc0b2482c6fde7039e7e9ab046fd088050f468
Haris Mohanty <harismohanty8658@gmail.com>
2023-03-05 18:33:14 +0530

    EMPLOYE page:- created a (dynamic_pages) file for store all dynamic file.

--- Employee/js/script.js
+++ Employee/js/script.js
@@ -8,6 +8,14 @@ $(document).ready(function () {
 
 //DYNAMIC REQUEST(CREATE CATEGORY) CODE START
 $(document).ready(function () {
-  
+  $(".collapse-item").each(function () {
+    $(this).click(function () {
+      let access_link = $(this).attr("access-link");
+      $.ajax({
+        type : "POST",
+        url : "dynamic_pages/"+access_link,
+      });
+    });
+  });
 });
 //DYNAMIC REQUEST(CREATE CATEGORY) CODE END
