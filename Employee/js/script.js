8db6c6ff7ce85d95325a30084212779b8ad3d379
Haris Mohanty <harismohanty8658@gmail.com>
2023-03-05 19:07:35 +0530

    EMPLOYEE page :- create a percentage progress bar code fixed.

--- Employee/js/script.js
+++ Employee/js/script.js
@@ -11,18 +11,39 @@ $(document).ready(function () {
   $(".collapse-item").each(function () {
     $(this).click(function () {
       let access_link = $(this).attr("access-link");
-      $.ajax({
-        type: "POST",
-        url: "dynamic_pages/" + access_link,
-        beforeSend : function(){
-          let progress
-          $(".page").html()
-        },
-        success: function (response) {
-          $(".page").html(response);
-        }
-      });
+      //create a function
+      dynamic_request(access_link);
     });
   });
 });
+
+//function call
+function dynamic_request(access_link) {
+  $.ajax({
+    type: "POST",
+    url: "dynamic_pages/" + access_link,
+    //for progress bar loading code start
+    xhr: function () {
+      let request = new XMLHttpRequest();
+      request.onprogress = function (e) {
+        let percentage = Math.floor((e.loaded * 100) / e.total);
+        let progress = `<div class="progress" role="progressbar" aria-label="Success striped example" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">
+                               <div class="progress-bar progress-bar-striped bg-success" style="width: ${percentage}%"></div>
+                      </div>`;
+      $(".page").html(progress);
+      };
+      return request;
+    },
+    //for progress bar loading code end
+    beforeSend: function () {
+      let progress = `<div class="progress" role="progressbar" aria-label="Success striped example" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">
+                               <div class="progress-bar progress-bar-striped bg-success" style="width: 25%"></div>
+                      </div>`;
+      $(".page").html(progress);
+    },
+    success: function (response) {
+      $(".page").html(response);
+    },
+  });
+}
 //DYNAMIC REQUEST(CREATE CATEGORY) CODE END
