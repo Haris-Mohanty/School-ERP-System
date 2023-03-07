1eee029e9bf96e0a2f8144b77d77d015b03e9404
Haris Mohanty <harismohanty8658@gmail.com>
2023-03-07 08:36:24 +0530

    Json string covert in to json-object/array and store in a variable successfully.

---
+++ Employee/php/create_category.php
@@ -0,0 +1,8 @@
+<?php
+
+require_once("../../Common_files/php/database.php");
+
+$category = json_decode($_POST['category']);
+$details = json_decode($_POST['details']);
+
+?>
--- Employee/php/database.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-$db = new mysqli("localhost", "root", "8658763042@Haris", "School_ERP");
-
-?>
