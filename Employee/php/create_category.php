a0d6d23d64dc6e2343f54718ca7d8f3e8067f06e
Haris Mohanty <harismohanty8658@gmail.com>
2023-03-07 08:39:16 +0530

    Create dynamic table for database code processing...

--- Employee/php/create_category.php
+++ Employee/php/create_category.php
@@ -5,4 +5,8 @@ require_once("../../Common_files/php/database.php");
 $category = json_decode($_POST['category']);
 $details = json_decode($_POST['details']);
 
+//get dynamic table
+$get_category = "SELECT * FROM category";
+
+
 ?>
