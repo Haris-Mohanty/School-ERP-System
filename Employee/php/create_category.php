bc65c87952f99e5dc3f9c7e9ae43b4e1c4317b2b
Haris Mohanty <harismohanty8658@gmail.com>
2023-03-07 08:48:18 +0530

    Create a table for database successful.

--- Employee/php/create_category.php
+++ Employee/php/create_category.php
@@ -7,6 +7,21 @@ $details = json_decode($_POST['details']);
 
 //get dynamic table
 $get_category = "SELECT * FROM category";
-
+$response = $db -> query($get_category);
+if($response){
+    echo "Table Found";
+}else{
+    $create_table = "CREATE TABLE category(
+        id INT(11) NOT NULL AUTO_INCREMENT,
+        category_name VARCHAR(55),
+        details VARCHAR(255),
+        PRIMARY KEY(id)
+    )";
+    if($db -> query($create_table)){
+        echo "table created.";
+    }else{
+        echo "faild";
+    }
+}
 
 ?>
