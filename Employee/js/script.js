da9fc9a83b8b1a08685beedfe2c8ea406c3029f9
Haris Mohanty <harismohanty8658@gmail.com>
2023-03-19 15:06:15 +0530

    EMPLOYEE page :- Create Course - COURSE LIST - Course list showing in table code fixed.

--- Employee/js/script.js
+++ Employee/js/script.js
@@ -347,18 +347,18 @@ function createCourseFunc() {
               all_data.forEach((data, index) => {
                 let tr = `
                 <tr index="${data.id}"> 
-                  <td class="text-nowrap">1</td>
-                  <td class="text-nowrap">MCA</td>
-                  <td class="text-nowrap">12452</td>
-                  <td class="text-nowrap">CSE</td>
-                  <td class="text-nowrap">4</td>
-                  <td class="text-nowrap">yr</td>
-                  <td class="text-nowrap">4579</td>
-                  <td class="text-nowrap">1-time</td>
-                  <td class="text-nowrap">Active</td>
-                  <td class="text-nowrap">12-05-2022</td>
-                  <td class="text-nowrap">Haris Mohanty</td>
-                  <td class="text-nowra">Lorem ipsum dolor sit, amet.</td>
+                  <td class="text-nowrap">${index+1}</td>
+                  <td class="text-nowrap">${data.category}</td>
+                  <td class="text-nowrap">${data.code}</td>
+                  <td class="text-nowrap">${data.name}</td>
+                  <td class="text-nowrap">${data.duration}</td>
+                  <td class="text-nowrap">${data.course_time}</td>
+                  <td class="text-nowrap">${data.fee}</td>
+                  <td class="text-nowrap">${data.fee_time}</td>
+                  <td class="text-nowra">${data.status}</td>
+                  <td class="text-nowrap">14-05-2020</td>
+                  <td class="text-nowrap">${data.added_by}</td>
+                  <td class="text-nowrap">${data.detail}</td>
                   <td class="text-nowrap">
                     <button class="btn btn-primary p-1 px-2"><i class="fa fa-edit"></i></button>
                     <button class="btn btn-danger p-1 px-2"><i class="fa fa-trash"></i></button>
