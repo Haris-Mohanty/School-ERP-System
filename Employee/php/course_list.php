2c2bd92199869d56695b0ca2d41a14b4691ad223
Haris Mohanty <harismohanty8658@gmail.com>
2023-03-19 14:19:55 +0530

    EMPLOYEE page :- Create Course - COURSE LIST -course_list.php- check response.

--- Employee/php/course_list.php
+++ Employee/php/course_list.php
@@ -6,6 +6,16 @@ require_once("../../Common_files/php/database.php");
 $category = $_POST['category'];
 $get_course = "SELECT * FROM course WHERE category = '$category'";
 
+$response = $db -> query($get_course);
 
+if($response){
+    if($response -> num_rows != 0){
+
+    }else{
+        
+    }
+}else{
+    echo "There is No Data Found!";
+}
 
 ?>
