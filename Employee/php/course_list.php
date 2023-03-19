37600295f17fddda606f0082dd0e7ee77c92a321
Haris Mohanty <harismohanty8658@gmail.com>
2023-03-19 14:16:04 +0530

    EMPLOYEE page :- Create Course - COURSE LIST -course_list.php- get_course sql query write successfully.

--- Employee/php/course_list.php
+++ Employee/php/course_list.php
@@ -3,6 +3,9 @@
 //LINK DATABASE
 require_once("../../Common_files/php/database.php");
 
+$category = $_POST['category'];
+$get_course = "SELECT * FROM course WHERE category = '$category'";
+
 
 
 ?>
