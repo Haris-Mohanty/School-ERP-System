2dffc1ad0e1ebfa3ebabcbfeecbbfe935809c70d
Haris Mohanty <harismohanty8658@gmail.com>
2023-03-20 15:41:48 +0530

    EMPLOYEE page :- Create Batch - get_all_course.php - get course sql query fixed.

--- Employee/php/get_all_course.php
+++ Employee/php/get_all_course.php
@@ -13,7 +13,14 @@ $response = $db -> query($get_course);
 $all_data = [];
 
 if($response){
-    
+    if($response -> num_rows != 0){
+        while($data = $response -> fetch_assoc()){
+            array_push($all_data, $data);
+        }
+        echo json_encode($all_data);
+    }else{
+        echo "There is No Course Found!";
+    }
 }else{
     echo "There is No Course Found!";
 }
