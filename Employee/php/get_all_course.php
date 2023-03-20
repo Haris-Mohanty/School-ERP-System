950926f75bfe38d54eca82a941497de83d63a1d7
Haris Mohanty <harismohanty8658@gmail.com>
2023-03-20 15:38:49 +0530

    EMPLOYEE page :- Create Batch - get_all_course.php - get course sql query processing..

--- Employee/php/get_all_course.php
+++ Employee/php/get_all_course.php
@@ -3,4 +3,19 @@
 //LINK DATABASE
 require_once("../../Common_files/php/database.php");
 
+$table = $_POST['table'];
+$category = $_POST['category'];
+
+$get_course = "SELECT name FROM $table WHERE category = '$category'"; //name-column name
+
+$response = $db -> query($get_course);
+
+$all_data = [];
+
+if($response){
+    
+}else{
+    echo "There is No Course Found!";
+}
+
 ?>
