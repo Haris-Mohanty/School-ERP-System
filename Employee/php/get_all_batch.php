dc3155b4c3758ca1ad9832d646fbba69af1c19b3
Haris Mohanty <harismohanty8658@gmail.com>
2023-03-21 11:54:39 +0530

    EMPLOYEE page :- Batch List - get_all_batch.php - sql query processing...

--- Employee/php/get_all_batch.php
+++ Employee/php/get_all_batch.php
@@ -3,6 +3,27 @@
 //LINK DATABASE
 require_once("../../Common_files/php/database.php");
 
+$table = $_POST['table'];
+$category = $_POST['category'];
+$course = $_POST['course'];
 
+$get_batch = "SELECT name FROM $table WHERE category = '$category' AND course = '$course'"; //name-column name
+
+$response = $db -> query($get_batch);
+
+$all_data = [];
+
+if($response){
+    if($response -> num_rows != 0){
+        while($data = $response -> fetch_assoc()){
+            array_push($all_data, $data);
+        }
+        echo json_encode($all_data);
+    }else{
+        echo "There is No Course Found!";
+    }
+}else{
+    echo "There is No Course Found!";
+}
 
 ?>
