5c9170db7f8d77e5e495ed40034ae10fa1031d12
Haris Mohanty <harismohanty8658@gmail.com>
2023-03-21 09:35:46 +0530

    EMPLOYEE page :- Create Batch - create_batch.php - Create a variable and store the name data in variable.

--- Employee/php/create_batch.php
+++ Employee/php/create_batch.php
@@ -1,1 +1,16 @@
-<?php?>
+<?php
+
+//LINK DATABASE
+require_once("../../Common_files/php/database.php");
+
+$category = $_POST['batch-category'];
+$course = $_POST['batch-course'];
+$batch_code = $_POST['batch-code'];
+$batch_name = $_POST['batch-name'];
+$detail = $_POST['batch-detail'];
+$batch_from = $_POST['batch-from'];
+$batch_to = $_POST['batch-to'];
+$batch_from_date = $_POST['batch-from-date'];
+$batch_to_date = $_POST['batch-to-date'];
+
+?>
