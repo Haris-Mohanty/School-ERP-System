d0c7212a36b059ffdff0f0335b786758b7eca8ed
Haris Mohanty <harismohanty8658@gmail.com>
2023-03-23 12:23:38 +0530

    EMPLOYEE page :- Batch List - update_batch.php - update query processing....

---
+++ Employee/php/update_batch.php
@@ -0,0 +1,51 @@
+<?php
+
+//LINK DATABASE
+require_once("../../Common_files/php/database.php");
+
+//receive the name of form data (course_design.php) page.  
+$category = $_POST['batch-category'];
+$course = $_POST['batch-course'];
+$batch_code = $_POST['batch-code'];
+$batch_name = $_POST['batch-name'];
+$detail = $_POST['batch-detail'];
+$batch_from = $_POST['batch-from'];
+$batch_to = $_POST['batch-to'];
+$batch_from_date = $_POST['batch-from-date'];
+$batch_to_date = $_POST['batch-to-date'];
+$file = $_FILES['batch-logo'];
+$batch_added_by = $_POST['batch-added-by'];
+$status = $_POST['status'];
+
+$logo = "";
+$name = "";
+$location = "";
+
+if($file['name'] == ""){
+    $logo = "";
+    $name = "";
+    $location = "";
+}else{
+    $name = $file['name'];
+    $location = $file['tmp_name'];
+    $logo = "Batch/".$name;
+}
+
+if($file['name'] == ""){
+    $update_batch = "UPDATE batch SET category = '$category', course = '$course', batch_code = '$batch_code', batch_name = '$batch_name', detail = '$detail', duration = '$duration', course_time = '$time', fee = '$fee', fee_time = '$fee_time', added_by = '$added_by', status = '$status' WHERE id = '$id'";
+
+    if($db -> query($update_course)){
+        echo "success";
+    }else{
+        echo "Unable to Update Course!";
+    }
+}else{
+     $update_course = "UPDATE course SET category = '$category', code = '$code', name = '$course_name', detail = '$detail', duration = '$duration', course_time = '$time', fee = '$fee', fee_time = '$fee_time', logo = '$logo', added_by = '$added_by', status = '$status' WHERE id = '$id'";
+     
+     if($db -> query($update_course)){
+         echo "success";
+     }else{
+         echo "Unable to Update Course!";
+     }
+}
+?>
