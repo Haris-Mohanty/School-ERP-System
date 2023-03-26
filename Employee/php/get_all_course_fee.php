dd82cd47e86e170c22c03d7a6ba5c304c3a3be4e
Haris Mohanty <harismohanty8658@gmail.com>
2023-03-26 09:58:33 +0530

    EMPLOYEE page :- Student Registration- get_all-course_fee.php - sql query fixed.

--- Employee/php/get_all_course_fee.php
+++ Employee/php/get_all_course_fee.php
@@ -3,4 +3,28 @@
 //LINK DATABASE
 require_once("../../Common_files/php/database.php");
 
+$table = $_POST['table'];
+$category = $_POST['category'];
+$course = $_POST['course'];
+
+$get_fee = "SELECT fee, fee_time FROM $table WHERE category = '$category' AND name = '$course'"; //name-column name
+
+$response = $db -> query($get_fee);
+
+$all_data = [];
+
+if($response){
+    if($response -> num_rows != 0){
+        
+        $data =  $response -> fetch_assoc();
+        echo json_encode($data);
+
+    }else{
+        echo "There is No Course Found!";
+    }
+}else{
+    echo "There is No Course Found!";
+}
+
+
 ?>
