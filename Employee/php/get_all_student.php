40f487585838d966e5d65d4c6974f862f2b121de
Haris Mohanty <harismohanty8658@gmail.com>
2023-03-28 20:17:41 +0530

    EMPLOYEE page :- get_all_student - query runed...

--- Employee/php/get_all_student.php
+++ Employee/php/get_all_student.php
@@ -7,10 +7,23 @@ $table = $_POST['table'];
 $category = $_POST['category'];
 $batch = $_POST['batch'];
 
-$get_students = "SELECT * FROM $table WHERE category = '$category' AND batch_name = '$batch'"; //name-column name
+$get_students = "SELECT * FROM $table WHERE category = '$category' AND batch = '$batch'"; //name-column name
 
 $response = $db -> query($get_students);
 
 $all_data = [];
+
+if($response){
+    if($response -> num_rows != 0){
+        while($data = $response -> fetch_assoc()){
+            array_push($all_data, $data);
+        }
+        echo json_encode($all_data);
+    }else{
+        echo "There is No Batch Found!";
+    }
+}else{
+    echo "There is No Batch Found!";
+}
 
 ?>
