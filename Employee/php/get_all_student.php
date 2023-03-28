99e0c5011df0833f0092fd597a83898c56f4546b
Haris Mohanty <harismohanty8658@gmail.com>
2023-03-28 20:11:58 +0530

    EMPLOYEE page :- get_all_student - query processing....

--- Employee/php/get_all_student.php
+++ Employee/php/get_all_student.php
@@ -3,4 +3,14 @@
 //LINK DATABASE
 require_once("../../Common_files/php/database.php");
 
+$table = $_POST['table'];
+$category = $_POST['category'];
+$batch = $_POST['batch'];
+
+$get_students = "SELECT * FROM $table WHERE category = '$category' AND batch_name = '$batch'"; //name-column name
+
+$response = $db -> query($get_students);
+
+$all_data = [];
+
 ?>
