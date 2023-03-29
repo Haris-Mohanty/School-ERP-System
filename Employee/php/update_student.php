39e63be0900efb574c8bd600a2ca85facc9587d8
Haris Mohanty <harismohanty8658@gmail.com>
2023-03-29 12:56:09 +0530

    EMPLOYEE page :- Registered Students List - update_student.php - Get the name variable.

--- Employee/php/update_student.php
+++ Employee/php/update_student.php
@@ -3,4 +3,25 @@
 //Link database
 require_once("../../Common_files/php/database.php");
 
+$category = $_POST['stu-category'];
+$course = $_POST['stu-course'];
+$batch = $_POST['stu-batch'];
+$enrollment = $_POST['enrollment'];
+$student_name = $_POST['name'];
+$dob = $_POST['dob'];
+$gender = $_POST['gender'];
+$father = $_POST['father'];
+$mother = $_POST['mother'];
+$email = $_POST['email'];
+$password = $_POST['password'];
+$mobile = $_POST['mobile'];
+$country = $_POST['country'];
+$state = $_POST['state'];
+$city = $_POST['city'];
+$pincode = $_POST['pincode'];
+$fee = $_POST['fee'];
+$fee_time = $_POST['fee-time'];
+$status = $_POST['status'];
+$added_by = $_POST['stu-added-by'];
+
 ?>
