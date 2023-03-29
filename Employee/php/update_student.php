60285f9c7bfee1e5f57b6383bcf0237fa19d4bc2
Haris Mohanty <harismohanty8658@gmail.com>
2023-03-29 13:07:03 +0530

    EMPLOYEE page :- Registered Students List - update_student.php - Update query fixed.

--- Employee/php/update_student.php
+++ Employee/php/update_student.php
@@ -25,7 +25,13 @@ $status = $_POST['status'];
 $added_by = $_POST['stu-added-by'];
 $id = $_POST['id'];
 
-$update_student = "UPDATE students SET category = '$category', course = '$course', batch = '$batch', enrollment = '$enrollment', student_name = '$student_name', dob = '$dob', gender = '$gender', "
+$update_student = "UPDATE students SET category = '$category', course = '$course', batch = '$batch', enrollment = '$enrollment', student_name = '$student_name', dob = '$dob', gender = '$gender', father = '$father', mother = '$mother', email = '$email', password = '$password', mobile = '$mobile', country = '$country', state = '$state', city = '$city', pincode = '$pincode', fee = '$fee', fee_time = '$fee_time', status = '$status', added_by = '$added_by' WHERE id = '$id'";
+
+if($db -> query($update_student)){
+    echo "success";
+}else{
+    echo "Unable to Update Student!";
+}
 
 
 ?>
