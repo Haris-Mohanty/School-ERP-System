fd7cfbf52670f45064edf8c32f9e9b7e49c1b65d
Haris Mohanty <harismohanty8658@gmail.com>
2023-03-31 14:11:34 +0530

    EMPLOYEE page :- Student Registration - Get_column_data.php - query processing...

--- Employee/php/get_column_data.php
+++ Employee/php/get_column_data.php
@@ -3,6 +3,12 @@
 //LINK DATABASE
 require_once("../../Common_files/php/database.php");
 
+$table = $_POST['table'];
+$column = $_POST['column'];
+$data = $_POST['user_data'];
+
+$get_data = "SELECT $column FROM $table WHERE $column = '$data'";
+
 
 
 ?>
