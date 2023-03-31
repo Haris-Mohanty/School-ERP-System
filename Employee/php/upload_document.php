f3d4426ce8247d5243b0c8a30e136c194ca7296f
Haris Mohanty <harismohanty8658@gmail.com>
2023-03-31 15:07:18 +0530

    EMPLOYEE page :- Upload Student Document - upload_document.php - All variable created.

--- Employee/php/upload_document.php
+++ Employee/php/upload_document.php
@@ -2,5 +2,9 @@
 
 //LINK DATABASE
 require_once("../../Common_files/php/database.php");
+$enrollment = $_POST['enrollment'];
+$photo = $_FILES['photo'];
+$signature = $_FILES['signature'];
+$id_proof = $_FILES['id-proof'];
 
 ?>
