16097f3b3f2471d21b80f9a8fae824eef3844be6
Haris Mohanty <harismohanty8658@gmail.com>
2023-03-31 18:11:20 +0530

    EMPLOYEE page :- Upload Student Document - upload_document.php - Update in students table query processing......

--- Employee/php/upload_document.php
+++ Employee/php/upload_document.php
@@ -14,7 +14,10 @@ $proof_location = $id_proof['tmp_name'];
 //Prepare URL for update in DATABASE
 $photo_url = "Photo/stu_".$enrollment.".png";
 $signature_url = "Signature/stu_".$enrollment.".png";
-$proof_url = "Document/stu_".$enrollment.".png";
+$proof_url = "Document/stu_".$enrollment.".pdf";
+
+//update in student table
+$update_student = "UPDATE students SET photo = '$photo_url'"
 
 
 ?>
