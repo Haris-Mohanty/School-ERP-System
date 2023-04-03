7e0a44fcfa742eac8d7a2ce37ef9b4cc5f6a8ec8
Haris Mohanty <harismohanty8658@gmail.com>
2023-04-03 19:04:02 +0530

    EMPLOYEE page :- Create Invoice - Add Invoice -add date in invoice code fixed.

--- Employee/php/create_invoice.php
+++ Employee/php/create_invoice.php
@@ -10,6 +10,7 @@ $course = $_POST['invoice-course'];
 $batch = $_POST['invoice-batch'];
 $paid_fee = $_POST['paid_fee'];
 $pending = $_POST['pending'];
+$date = $_POST['date'];
 
 $fee_time = $_POST['invoice-fee-time'];
 $invoice_pending = $_POST['invoice-pending'];
@@ -19,7 +20,7 @@ $get_data = "SELECT * FROM invoice";
 
 $response = $db -> query($get_data);
 if($response){
-    $insert_data = "INSERT INTO invoice(enrollment, name, category, course, batch, paid_fee, pending, fee_time, invoice_pending, invoice_recent) VALUES('$enrollment', '$name', '$category', '$course', '$batch', '$paid_fee', '$pending', '$fee_time', '$invoice_pending', '$invoice_recent')";
+    $insert_data = "INSERT INTO invoice(enrollment, name, category, course, batch, paid_fee, pending, fee_time, invoice_pending, invoice_recent, date) VALUES('$enrollment', '$name', '$category', '$course', '$batch', '$paid_fee', '$pending', '$fee_time', '$invoice_pending', '$invoice_recent', '$date')";
     if($db -> query($insert_data)){
 
         $update_student = "UPDATE students SET paid_fee = '$paid_fee' WHERE enrollment = '$enrollment'";
@@ -41,10 +42,11 @@ if($response){
         fee_time VARCHAR(55),
         invoice_pending VARCHAR(55),
         invoice_recent VARCHAR(55),
+        date VARCHAR(55),
         PRIMARY KEY(id)
     )";
     if($db -> query($create_table)){
-        $insert_data = "INSERT INTO invoice(enrollment, name, category, course, batch, paid_fee, pending, fee_time, invoice_pending, invoice_recent) VALUES('$enrollment', '$name', '$category', '$course', '$batch', '$paid_fee', '$pending', '$fee_time', '$invoice_pending', '$invoice_recent')";
+        $insert_data = "INSERT INTO invoice(enrollment, name, category, course, batch, paid_fee, pending, fee_time, invoice_pending, invoice_recent, date) VALUES('$enrollment', '$name', '$category', '$course', '$batch', '$paid_fee', '$pending', '$fee_time', '$invoice_pending', '$invoice_recent', '$date')";
         if($db -> query($insert_data)){
 
            $update_student = "UPDATE students SET paid_fee = '$paid_fee' WHERE enrollment = '$enrollment'";
