dadc9ac3fc9e7d78e636fe723cee3563daf36697
Haris Mohanty <harismohanty8658@gmail.com>
2023-04-03 09:10:31 +0530

    EMPLOYEE page :- Create Invoice - Add Invoice - create_invoice.php - All variable created successfully.

--- Employee/php/create_invoice.php
+++ Employee/php/create_invoice.php
@@ -3,5 +3,12 @@
 //LINK DATABASE
 require_once("../../Common_files/php/database.php");
 
+$enrollment = $_POST['invoice-enrollment'];
+$name = $_POST['invoice-name'];
+$category = $_POST['invoice-category'];
+$course = $_POST['invoice-course'];
+$batch = $_POST['invoice-batch'];
+$paid_fee = $_POST['paid-fee'];
+$pending = $_POST['pending'];
 
 ?>
