3008746f756c51b6a60cec3391280976f8fef4d6
Haris Mohanty <harismohanty8658@gmail.com>
2023-04-04 13:48:06 +0530

    EMPLOYEE page :- Branding Details - Create brand - create_brand.php - All variable created successfully.

--- Employee/php/create_brand.php
+++ Employee/php/create_brand.php
@@ -3,4 +3,20 @@
 //LINK DATABASE
 require_once("../../Common_files/php/database.php");
 
+$brand_name = $_POST['brand-name']
+$brand_domain = $_POST['brand-domain']
+$brand_email = $_POST['brand-email']
+$brand_twitter = $_POST['brand-twitter']
+$brand_facebook = $_POST['brand-facebook']
+$brand_instagram = $_POST['brand-instagram']
+$brand_whatsapp = $_POST['brand-whatsapp']
+$brand_address = $_POST['brand-address']
+$brand_mobile = $_POST['brand-mobile']
+$brand_about = $_POST['brand-about']
+$brand_privacy = $_POST['brand-privacy']
+$brand_cookie = $_POST['brand-cookie']
+$brand_terms = $_POST['brand-terms']
+
+$brand_logo = $_FILES['brand-logo']
+
 ?>
