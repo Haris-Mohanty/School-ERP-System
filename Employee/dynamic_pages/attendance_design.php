26bb767043dcee3d124cd0f72b475be0fc5ce78c
Haris Mohanty <harismohanty8658@gmail.com>
2023-04-08 13:21:54 +0530

    EMPLOYEE page :- Student Attendance - attendance_design.php - Choose category show category code processing..

--- Employee/dynamic_pages/attendance_design.php
+++ Employee/dynamic_pages/attendance_design.php
@@ -6,6 +6,21 @@ require_once("../../Common_files/php/database.php");
 // NAV LINK
 require_once("../php/nav.php");
 
+//choose category
+$get_category = "SELECT * FROM category";
+
+$response = $db -> query($get_category);
+
+$all_category = [];
+
+if($response){
+  while($data = $response -> fetch_assoc()){
+    array_push($all_category , $data['category_name']);
+  }
+}
+
+$length = count($all_category);
+
 echo '
 
 <!-- STUDENT ATTENDANCE - CODE START -->
