0fdf545064cc37653a98ac07471b7e00cfbdc15f
Haris Mohanty <harismohanty8658@gmail.com>
2023-04-09 15:35:39 +0530

    EMPLOYEE page :- Student Attendance - store attendance - create_attendance.php- Create table query processing..

--- Employee/php/create_attendance.php
+++ Employee/php/create_attendance.php
@@ -8,4 +8,19 @@ $name = json_decode($_POST['name']);
 $batch = json_decode($_POST['batch']);
 $attendance = json_decode($_POST['attendance']);
 
+$get_att = "SELECT * FROM attendance";
+
+$response = $db -> query($get_att);
+if($response){
+
+    echo "Found";
+
+}else{
+
+    $create_table = "CREATE TABLE attendance(
+        
+    )";
+
+}
+
 ?>
