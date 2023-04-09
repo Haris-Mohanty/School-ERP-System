a74d3c37e7ac76c6f2f38e83ffb1b6c43c70e29c
Haris Mohanty <harismohanty8658@gmail.com>
2023-04-09 15:33:18 +0530

    EMPLOYEE page :- Student Attendance - store attendance - create_attendance.php- all variable created.

--- Employee/php/create_attendance.php
+++ Employee/php/create_attendance.php
@@ -3,4 +3,9 @@
 //LINK DATABASE
 require_once("../../Common_files/php/database.php");
 
+$enrollment = json_decode($_POST['enrollment']);
+$name = json_decode($_POST['name']);
+$batch = json_decode($_POST['batch']);
+$attendance = json_decode($_POST['attendance']);
+
 ?>
