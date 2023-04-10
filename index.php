de3ada8e0b68cce0d50d210584c14b1075debd8a
Haris Mohanty <harismohanty8658@gmail.com>
2023-04-10 15:52:57 +0530

    Home page : Index.php - Footer code - show contacts code fixed.

--- index.php
+++ index.php
@@ -92,14 +92,12 @@ require("Common_files/php/database.php");
                     </ul>
                 </div>
                 <div class="col-md-1"></div>
-                <div class="col-md-4">
+                <div class="col-md-4 mb-3">
                 <h4 class="text-white">CONTACT</h4>
-                    <ul class="list-group">
-                        <li class="list-group-item text-white bg-transparent" style="cursor: pointer;">Privacy Policy</li>
-                        <li class="list-group-item text-white bg-transparent" style="cursor: pointer;">Cookie Policy</li>
-                        <li class="list-group-item text-white bg-transparent" style="cursor: pointer;">Terms & Conditions</li>
-                        <li class="list-group-item text-white bg-transparent" style="cursor: pointer;">About Us</li>
-                    </ul>
+                    <p class="text-white py-2"> <b>Venue</b>: </p>
+                    <p class="text-white py-2"> <b>Call us</b>: </p>
+                    <p class="text-white py-2"> <b>Email</b>: </p>
+                    <p class="text-white py-2"> <b>Website</b>: </p>
                 </div>
             </div>
         </div>
