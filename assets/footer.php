3b4aadde8c874d50c0c833de5ec958b9e0ac3c65
Haris Mohanty <harismohanty8658@gmail.com>
2023-04-11 20:34:44 +0530

    Home page : Index.php - footer - Show contact code processing....

--- assets/footer.php
+++ assets/footer.php
@@ -4,7 +4,7 @@
                 <div class="col-md-6 mb-2 d-flex justify-content-center align-items-center">
                     <div class="input-group w-100">
                         <input type="email" name="" placeholder="email@gmail.com" id="" class="form-control">
-                        <span class="input-group-text">Subscribe</span>
+                        <span class="input-group-text" style="cursor: pointer;">Subscribe</span>
                     </div>
                 </div>
                 <div class="col-md-6 mb-2 d-flex justify-content-center align-items-center">
@@ -45,7 +45,7 @@
                 <div class="col-md-1"></div>
                 <div class="col-md-4 mb-3">
                 <h4 class="text-white">CONTACT</h4>
-                    <p class="text-white py-2"> <b>Venue</b>: </p>
+                    <p class="text-white py-2"> <b>Venue</b>: <?php echo $brand_res['brand_address']?> </p>
                     <p class="text-white py-2"> <b>Call us</b>: </p>
                     <p class="text-white py-2"> <b>Email</b>: </p>
                     <p class="text-white py-2"> <b>Website</b>: </p>
