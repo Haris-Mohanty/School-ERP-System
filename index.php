c209912316c0281ebe6ab228dc3975a68ae280df
Haris Mohanty <harismohanty8658@gmail.com>
2023-04-12 11:54:35 +0530

    Home page : Index.php - carousal code processing....

--- index.php
+++ index.php
@@ -49,6 +49,34 @@ require("Common_files/php/database.php");
     ?>
 
     <!-- Main Page code start -->
+  <div class="container-fluid">
+    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+       <ol class="carousel-indicators">
+         <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+         <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+         <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+       </ol>
+       <div class="carousel-inner">
+         <div class="carousel-item active">
+           <img class="d-block w-100" src="..." alt="First slide">
+         </div>
+         <div class="carousel-item">
+           <img class="d-block w-100" src="..." alt="Second slide">
+         </div>
+         <div class="carousel-item">
+           <img class="d-block w-100" src="..." alt="Third slide">
+         </div>
+       </div>
+       <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+         <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+         <span class="sr-only">Previous</span>
+       </a>
+       <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+         <span class="carousel-control-next-icon" aria-hidden="true"></span>
+         <span class="sr-only">Next</span>
+       </a>
+    </div>
+ </div>
     <!-- Main Page code end -->
 
     <!-- footer -->
