28baa516673d1971f71484a4013b8654711b1995
Haris Mohanty <harismohanty8658@gmail.com>
2023-04-14 10:36:30 +0530

    Home page : add about us link to nav.

--- assets/nav.php
+++ assets/nav.php
@@ -31,7 +31,7 @@
                             <a href="#" class="nav-link">Home</a>
                         </li>
                         <li class="nav-item">
-                            <a href="#" class="nav-link">About</a>
+                            <a href="aboutus.php" class="nav-link">About</a>
                         </li>
                         <?php
                         
