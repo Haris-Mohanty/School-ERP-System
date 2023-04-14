98b15051ff07124fa8be83ef473d34cfd588d5ae
Haris Mohanty <harismohanty8658@gmail.com>
2023-04-14 10:19:30 +0530

    Home page : cookie.php - sql query fixed.

---
+++ cookie.php
@@ -0,0 +1,62 @@
+<?php
+
+require("Common_files/php/database.php");
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- BOOTSTRAP CSS LINK START -->
+    <link rel="stylesheet" href="Common_files/css/bootstrap.min.css" />
+    <!-- BOOTSTRAP CSS LINK END -->
+
+    <!-- BOOTSTRAP JS LINK START -->
+    <script src="Common_files/js/bootstrap.bundle.min.js"></script>
+    <!-- BOOTSTRAP JS LINK END -->
+
+    <!-- ANIMATE CSS LINK START -->
+    <link rel="stylesheet" href="Common_files/css/animate.min.css" />
+    <!-- ANIMATE CSS LINK END -->
+
+    <!-- SWEET ALERT JS LINK START -->
+    <script src="Common_files/js/sweetalert.min.js"></script>
+    <!-- SWEET ALERT JS LINK END -->
+
+    <!--FONT AWESOME LINK START -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ==" crossorigin="anonymous" referrerpolicy="no-referrer"/>
+    <!--FONT AWESOME LINK END -->
+
+    <!-- JQUERY LINK START -->
+    <script src="Common_files/js/jquery.min.js"></script>
+    <!-- JQUERY LINK END -->
+
+    <!--   GOOGLE FONT START -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Sofia"/>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Audiowide"/>
+    <!--   GOOGLE FONT END -->
+
+    <title>School ERP System</title>
+    <link rel="stylesheet" href="pages/index.css">
+</head>
+<body>
+    <!-- nav -->
+    <?php
+        require("assets/nav.php");
+    ?>
+
+    <div class="container shadow-lg bg-white p-4 border" style="margin-top: 100px;">
+        <?php
+            echo $brand_res['brand_cookie'];
+        ?>
+    </div>
+
+    <!-- footer -->
+    <?php require("assets/footer.php"); ?>
+     
+</body>
+</html>
