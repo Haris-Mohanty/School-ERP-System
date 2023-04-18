ff2bdce6c5bd16cf30ccd5cd9e33135281da7b86
Haris Mohanty <harismohanty8658@gmail.com>
2023-04-18 22:02:45 +0530

    Home page : login.php - query processing....

--- pages/login.php
+++ pages/login.php
@@ -9,7 +9,15 @@ $user = $_POST['user'];
 if($user == "admin"){
     echo "Admin";
 }else{
-    echo "Student";
+    $get_email = "SELECT email FROM students WHERE email = '$email'";
+
+    $email_res = $db -> query($get_email);
+    
+    if($email_res -> num_rows){
+        echo "s";
+    }else{
+        echo "Wrong Username";
+    }
 }
 
 ?>
