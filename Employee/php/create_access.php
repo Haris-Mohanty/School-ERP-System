e0de8a7286a8ea0dbb7c44074aa8d1f3e1b1020b
Haris Mohanty <harismohanty8658@gmail.com>
2023-04-19 08:43:57 +0530

    EMPLOYEE page :- Give Access Link - create_access.php - Create table code processing.......

--- Employee/php/create_access.php
+++ Employee/php/create_access.php
@@ -6,4 +6,16 @@ require_once("../../Common_files/php/database.php");
 $username = $_POST['username'];
 $password = $_POST['password'];
 
+$get_user = "SELECT * FROM access";
+
+$response = $db -> query($get_user);
+
+if($response){
+    echo "Found";
+
+}else{
+
+    $create_table = "";
+}
+
 ?>
