7679592a129e146efea5cbc2ef9781adc6eaa7dc
Haris Mohanty <harismohanty8658@gmail.com>
2023-04-24 18:49:51 +0530

    EMPLOYEE page :- Homepage Design - Add showcase in db - create_showcase.php - Decode object.

--- Employee/php/create_showcase.php
+++ Employee/php/create_showcase.php
@@ -3,7 +3,12 @@
 //LINK DATABASE
 require_once("../../Common_files/php/database.php");
 
-print_r($_FILES['file_data']);
-print_r($_POST['css_data']);
+// $file = $_FILES['file_data'];
+$json_data = json_encode($_POST['css_data']); //receive by encoding
+$tmp_data = json_decode($json_data, true);
+$all_data = json_decode($tmp_data, true);
+print_r($all_data);
+
+
 
 ?>
