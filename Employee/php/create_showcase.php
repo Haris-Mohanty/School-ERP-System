d902e54f66e45f09f27942d9d3566a5e358693c9
Haris Mohanty <harismohanty8658@gmail.com>
2023-04-24 18:56:10 +0530

    EMPLOYEE page :- Homepage Design - Add showcase in db - create_showcase.php - Store vaariable under addslashes.

--- Employee/php/create_showcase.php
+++ Employee/php/create_showcase.php
@@ -3,23 +3,23 @@
 //LINK DATABASE
 require_once("../../Common_files/php/database.php");
 
-// $file = $_FILES['file_data'];
+$file = $_FILES['file_data'];
 $json_data = json_encode($_POST['css_data']); //receive by encoding
 $tmp_data = json_decode($json_data, true);
 $all_data = json_decode($tmp_data, true);
 
-$title_text = $all_data['title_text'];
-$title_size = $all_data['title_size'];
-$title_color = $all_data['title_color'];
+$title_text = addslashes($all_data['title_text']);
+$title_size = addslashes($all_data['title_size']);
+$title_color = addslashes($all_data['title_color']);
 
-$subtitle_text = $all_data['subtitle_text'];
-$subtitle_size = $all_data['subtitle_size'];
-$subtitle_color = $all_data['subtitle_color'];
+$subtitle_text = addslashes($all_data['subtitle_text']);
+$subtitle_size = addslashes($all_data['subtitle_size']);
+$subtitle_color = addslashes($all_data['subtitle_color']);
 
 $h_align = $all_data['h_align'];
 $v_align = $all_data['v_align'];
 
-$buttons = $all_data['buttons'];
+$buttons = addslashes($all_data['buttons']);
 
 
 ?>
