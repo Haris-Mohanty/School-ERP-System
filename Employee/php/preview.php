a8c92d4029dd1e1181c90cb1c569b77e12c0b4d8
Haris Mohanty <harismohanty8658@gmail.com>
2023-04-25 10:11:46 +0530

    EMPLOYEE page :- Homepage Design - Preview button - preview.php - All variable created.

--- Employee/php/preview.php
+++ Employee/php/preview.php
@@ -3,6 +3,12 @@
 //LINK DATABASE
 require_once("../../Common_files/php/database.php");
 
+$file = $_FILES['photo'];
+$data = json_decode($_POST['data']);
+$text = $data[0];
+$h_align = $data[1];
+$v_align = $data[2];
+
 ?>
 
 <!DOCTYPE html>
@@ -50,65 +56,7 @@ require_once("../../Common_files/php/database.php");
         require("assets/nav.php");
     ?>
 
-    <!-- Main Page code start -->
-  <div class="container-fluid" style="margin-top: 90px;">
-  <!-- CAROUSAL CODE START -->
-    <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
-       <div class="carousel-inner">
-         <div class="carousel-item active">
-           <img class="d-block w-100" src="Images/mitt.png" alt="First slide">
-         </div>
-         <div class="carousel-item">
-           <img class="d-block w-100" src="Images/mc2.png" alt="Second slide">
-         </div>
-         <div class="carousel-item">
-           <img class="d-block w-100" src="Images/buss.jpg" alt="Third slide">
-         </div>
-       </div>
-       <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-         <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-         <span class="sr-only">Previous</span>
-       </a>
-       <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-         <span class="carousel-control-next-icon" aria-hidden="true"></span>
-         <span class="sr-only">Next</span>
-       </a>
-    </div>
-  <!-- CAROUSAL CODE END -->
 
-
-  <!-- COURSE SECTION CODE START-->
-   <div class="course-section">
-    <div class="container">
-      <h1 class="mt-5 fw-bold mb-4">Trending Courses</h1>
-      <div class="row mb-5">
-        <?php
-         $get_course = "SELECT * FROM course";
-         $course_res = $db -> query($get_course);
-         if($course_res){
-          while($data = $course_res -> fetch_assoc()){
-            echo '
-            <div class="col-md-3 p-0 mb-3 course-box">
-            <div class="card">
-              <img src="Employee/'.$data['logo'].'" class="card-img-top" alt="">
-              <div class="card-body">
-                <h4 class="card-title">
-                  '.$data['name'].'
-                </h4>
-                <p class="card-text fw-bold">'.$data['fee'].'</p>
-              </div>
-            </div>
-          </div>
-            ';
-          }
-         }
-        ?>
-      </div>
-    </div>
-   </div>
-  <!-- COURSE SECTION CODE END-->
-  </div>
-    <!-- Main Page code end -->
 
     <!-- footer -->
     <?php require("assets/footer.php"); ?>
