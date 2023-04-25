4dfd0c18d8a6c56d344e25a7c97f50befc1ca2a0
Haris Mohanty <harismohanty8658@gmail.com>
2023-04-25 15:36:02 +0530

    EMPLOYEE page :- Homepage Design - Edit all showcase -edit_showcase.php - sql query fixed.

--- Employee/php/edit_showcase.php
+++ Employee/php/edit_showcase.php
@@ -5,12 +5,31 @@ require_once("../../Common_files/php/database.php");
 
 $id = $_POST['id'];
 
-$get_data = "SELECT * FROM header_showcase";
+$get_data = "SELECT * FROM header_showcase WHERE id = '$id'";
 
 $response = $db -> query($get_data);
 
 if($response){
-    
+
+    $data = $response -> fetch_assoc();
+
+    $image = "data:image/png;base64,".base64_encode($data['title_image']);
+    $title_text = $data['title_text'];
+    $title_color = $data['title_color'];
+    $title_size = $data['title_size'];
+
+    $subtitle_text = $data['subtitle_text'];
+    $subtitle_color = $data['subtitle_color'];
+    $subtitle_size = $data['subtitle_size'];
+
+    $h_align = $data['h_align'];
+    $v_align = $data['v_align'];
+
+    $buttons = $data['buttons'];
+
+    $all_data = [$image, $title_text, $title_color, $title_size, $subtitle_text, $subtitle_color, $subtitle_size, $h_align, $v_align, $buttons];
+
+    echo json_encode($all_data);
 }
 
 ?>
