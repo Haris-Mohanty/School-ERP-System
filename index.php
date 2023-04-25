2ca2a6cef37717c605fa1f96021caee333c5b661
Haris Mohanty <harismohanty8658@gmail.com>
2023-04-25 14:47:56 +0530

    Home page :- Show showcasedesign in Home page code- Set align code fixed.

--- index.php
+++ index.php
@@ -64,11 +64,12 @@ require("Common_files/php/database.php");
             {
 
               $h_align = $data['h_align'];
+              $v_align = $data['v_align'];
 
               echo "<div class='carousel-item carousel-control'>";
                 $image = "data:image/png;base64,".base64_encode($data['title_image']);
                 echo "<img src='".$image."' class='w-100'>";
-                echo "<div class='carousel-caption d-flex' style='justify-content:".$h_align."'>";
+                echo "<div class='carousel-caption d-flex' style='justify-content:".$h_align."; align-items:".$v_align.";'>";
 
                echo "<div>";
                echo "<h1>".$data['title_text']."</h1>";
