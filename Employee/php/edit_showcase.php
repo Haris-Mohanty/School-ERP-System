5a1dcb150a5ecdbaa5442e820222e08318e7c9e8
Haris Mohanty <harismohanty8658@gmail.com>
2023-04-25 15:29:44 +0530

    EMPLOYEE page :- Homepage Design - Edit all showcase -edit_showcase.php - sql query processing..

--- Employee/php/edit_showcase.php
+++ Employee/php/edit_showcase.php
@@ -3,4 +3,14 @@
 //LINK DATABASE
 require_once("../../Common_files/php/database.php");
 
+$id = $_POST['id'];
+
+$get_data = "SELECT * FROM header_showcase";
+
+$response = $db -> query($get_data);
+
+if($response){
+    
+}
+
 ?>
