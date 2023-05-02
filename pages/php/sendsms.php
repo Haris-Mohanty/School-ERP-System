d442658fd5e39278edd6a5175d8a027b34159433
Haris Mohanty <harismohanty8658@gmail.com>
2023-05-02 13:44:47 +0530

    Register Page :- working on sendsms..

--- pages/php/sendsms.php
+++ pages/php/sendsms.php
@@ -1,10 +1,10 @@
 <?php
 require('textlocal.class.php');
 
-$textlocal = new Textlocal('demo@txtlocal.com', 'apidemo123');
+$textlocal = new Textlocal(false, false, 'NmE1MjZjNGU3NDcwNDU2Zjc5NzU3NTRjMzk0OTY2NjE=');
 
 $numbers = array(918123456789);
-$sender = 'Textlocal';
+$sender = '600010';
 $message = 'This is a message';
 
 try {
