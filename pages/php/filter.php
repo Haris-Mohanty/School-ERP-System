50abacf286249e8b53b348350deb80d7818e92a2
Haris Mohanty <harismohanty8658@gmail.com>
2023-05-05 13:33:20 +0530

    course.php - Show Course data - filter.php - Query processing..

--- pages/php/filter.php
+++ pages/php/filter.php
@@ -6,4 +6,14 @@ require("../../Common_files/php/database.php");
 $cat_name = $_POST['cat_name'];
 $course_name = $_POST['course_name'];
 
+$get_batch = "SELECT * FROM batch WHERE category = '$cat_name' AND course = '$course_name'";
+
+$response = $db -> query($get_batch);
+
+if($response){
+    echo "found";
+}else{
+    echo "";
+}
+
 ?>
