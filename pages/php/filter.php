82fc4bf1618ed62d22aaf4ccb9001ec909379933
Haris Mohanty <harismohanty8658@gmail.com>
2023-05-05 13:30:53 +0530

    course.php - Show Course data - filter.php - Variable created.

--- pages/php/filter.php
+++ pages/php/filter.php
@@ -1,6 +1,9 @@
 <?php
 
-    //Database linked
-    require("../../Common_files/php/database.php");
+//Database linked
+require("../../Common_files/php/database.php");
+
+$cat_name = $_POST['cat_name'];
+$course_name = $_POST['course_name'];
 
 ?>
