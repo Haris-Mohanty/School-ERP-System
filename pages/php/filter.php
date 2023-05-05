2a9b2c6c6aec86dd2dbbeb3ac799aab4b8a1675c
Haris Mohanty <harismohanty8658@gmail.com>
2023-05-05 13:36:03 +0530

    course.php - Show Course data - filter.php - Query fixed.

--- pages/php/filter.php
+++ pages/php/filter.php
@@ -5,15 +5,19 @@ require("../../Common_files/php/database.php");
 
 $cat_name = $_POST['cat_name'];
 $course_name = $_POST['course_name'];
+$batch_list = [];
 
 $get_batch = "SELECT * FROM batch WHERE category = '$cat_name' AND course = '$course_name'";
 
 $response = $db -> query($get_batch);
 
 if($response){
-    echo "found";
+    while($data = $response -> fetch_assoc()){
+        array_push($batch_list, $data);
+    }
+    echo json_encode($batch_list);
 }else{
-    echo "";
+    echo "There is no Batch!";
 }
 
 ?>
