230e36fc2559691e8068d56367b3c439c0805054
Haris Mohanty <harismohanty8658@gmail.com>
2023-05-12 09:12:49 +0530

    Employee Page :- Dashboard- invoice list-  payment -create_invoice - Check some error.

--- Student/php/create_invoice.php
+++ Student/php/create_invoice.php
@@ -1,7 +1,7 @@
 <?php
 
 //LINK DATABASE
-require_once("../../Common_files/php/database.php");
+require_once("../Common_files/php/database.php");
 
 $enrollment = $_SESSION['enrollment'];
 $name = $_SESSION['name'];
@@ -10,15 +10,15 @@ $course = $_SESSION['course'];
 $batch = $_SESSION['batch'];
 $paid_fee = $_SESSION['total'];
 $pending = $_SESSION['pending'];
-$fee_time = $_SESSION['fee-time'];
-$invoice_recent = $_POST['invoice-recent'];
+$fee_time = $_SESSION['fee_time'];
+$invoice_recent = $_SESSION['recent'];
 $date = $_SESSION['date'];
 
 $get_data = "SELECT * FROM invoice";
 
 $response = $db -> query($get_data);
 if($response){
-    $insert_data = "INSERT INTO invoice(enrollment, name, category, course, batch, paid_fee, pending, fee_time, invoice_recent, date) VALUES('$enrollment', '$name', '$category', '$course', '$batch', '$paid_fee', '$pending', '$fee_time', '$invoice_recent', '$date')";
+    $insert_data = "INSERT INTO invoice(enrollment, name, category, course, batch, paid_fee, pending, fee_time, invoice_pending, invoice_recent, date) VALUES('$enrollment', '$name', '$category', '$course', '$batch', '$paid_fee', '$pending', '$fee_time', '$pending', '$invoice_recent', '$date')";
     if($db -> query($insert_data)){
 
         $update_student = "UPDATE students SET paid_fee = '$paid_fee' WHERE enrollment = '$enrollment'";
@@ -38,12 +38,13 @@ if($response){
         paid_fee VARCHAR(55),
         pending VARCHAR(55),
         fee_time VARCHAR(55),
+        invoice_pending VARCHAR(55),
         invoice_recent VARCHAR(55),
         date VARCHAR(55),
         PRIMARY KEY(id)
     )";
     if($db -> query($create_table)){
-        $insert_data = "INSERT INTO invoice(enrollment, name, category, course, batch, paid_fee, pending, fee_time, invoice_recent, date) VALUES('$enrollment', '$name', '$category', '$course', '$batch', '$paid_fee', '$pending', '$fee_time', '$invoice_recent', '$date')";
+        $insert_data = "INSERT INTO invoice(enrollment, name, category, course, batch, paid_fee, pending, fee_time, invoice_pending, invoice_recent, date) VALUES('$enrollment', '$name', '$category', '$course', '$batch', '$paid_fee', '$pending', '$fee_time', '$pending', '$invoice_recent', '$date')";
         if($db -> query($insert_data)){
 
            $update_student = "UPDATE students SET paid_fee = '$paid_fee' WHERE enrollment = '$enrollment'";
